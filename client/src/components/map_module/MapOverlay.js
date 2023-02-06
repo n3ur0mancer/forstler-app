e546c6bfcc84606be56b25b0f55640919e24f06c
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-02-06 21:37:58 +0100

    Made the sun icon dynamic

--- client/src/components/map_module/MapOverlay.js
+++ client/src/components/map_module/MapOverlay.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
 import arrow from "./assets/arrow_icon.svg";
 import sunny_icon from "./assets/sunny_icon.svg";
+import sunny_shadow_icon from "./assets/sunny-shadow_icon.svg";
+import shadow_icon from "./assets/shadow_icon.svg";
 import "./css/map_overlay.css";
 
 function MapOverlay(props) {
+  let icon;
+
+  if (props.sunhours < 4) {
+    icon = shadow_icon;
+  } else if (props.sunhours >= 4 && props.sunhours <= 8) {
+    icon = sunny_shadow_icon;
+  } else {
+    icon = sunny_icon;
+  }
+
   return (
     <div className="map_overlay_container">
       <div className="elevation_container">
@@ -11,7 +23,7 @@ function MapOverlay(props) {
         <p className="elevation_text">{props.elevation} m</p>
       </div>
       <div className="sun_hours_container">
-        <img src={sunny_icon} className="sun_hours_image"></img>
+        <img src={icon} className="sun_hours_image"></img>
         <p className="sun_hours_text">{props.sunhours} h</p>
       </div>
     </div>
